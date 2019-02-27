9ae717f55fdb652bc79bbaba4d159653372abdd8
Luke Bordonaro <lukebordonaro@gmail.com>
2019-02-27 09:15:59 -0500

    initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+
+import { withFirebase } from '../Firebase';
+
+import Navigation from '../Navigation';
+import LandingPage from '../Landing';
+import SignUpPage from '../SignUp';
+import SignInPage from '../SignIn';
+import PasswordForgetPage from '../PasswordForget';
+import HomePage from '../Home';
+import AccountPage from '../Account';
+import AdminPage from '../Admin';
+
+import * as ROUTES from '../../constants/routes';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      authUser: null,
+    };
+  }
+
+	componentDidMount() {
+    this.props.firebase.auth.onAuthStateChanged(authUser => {
+      authUser
+        ? this.setState({ authUser })
+        : this.setState({ authUser: null });
+    });
+  }
+
+	componentWillUnmount() {
+    this.listener();
+  }
+
+	render() {
+    return (
+      <Router>
+        <div>
+          <Navigation authUser={this.state.authUser} />
+
+          <hr/>
+
+					<Route exact path={ROUTES.LANDING} component={LandingPage} />
+					<Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+					<Route path={ROUTES.SIGN_IN} component={SignInPage} />
+					<Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+					<Route path={ROUTES.HOME} component={HomePage} />
+					<Route path={ROUTES.ACCOUNT} component={AccountPage} />
+					<Route path={ROUTES.ADMIN} component={AdminPage} />
+				</div>
+			</Router>
+		);
+	}
+}
+
+export default withFirebase(App);
