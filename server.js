354ceda6d6b74331442ca95772865e41cafada66
Luke Bordonaro <lukebordonaro@gmail.com>
2020-02-09 17:30:34 -0500

    frgrde2qw

--- server.js
+++ server.js
@@ -6,12 +6,8 @@ const port = process.env.PORT || 8080;
 const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-app.get('*', function (req, res) {
-  res.sendFile(path.resolve(__dirname + '/../dist/index.html'));
+app.use('/static', express.static(path.join(__dirname, '../client/build//static')));
+app.get('*', function(req, res) {
+  res.sendFile('index.html', {root: path.join(__dirname, '../../client/build/')});
 });
 app.listen(port);
