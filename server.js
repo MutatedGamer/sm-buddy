a81ead907eb73c0e5a9145f153275b677945da4f
Luke Bordonaro <lukebordonaro@gmail.com>
2020-02-09 16:01:29 -0500

    update server.js

--- server.js
+++ server.js
@@ -14,4 +14,5 @@ app.get('/ping', function (req, res) {
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+res.sendFile(path.resolve(__dirname + '/../dist/index.html'));
 app.listen(port);
